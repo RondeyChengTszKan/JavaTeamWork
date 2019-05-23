9635b3b8bf84d8183d030cf6b1a6ed9b79c0c4c4
emikosa <emi.kosa@bizreach.co.jp>
2019-05-23 17:54:01 +0900

    add DAO

---
+++ src/la/dao/ItemDAO.java
@@ -0,0 +1,67 @@
+package la.dao;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import la.servlet.model.*;
+
+
+public class ItemDAO {
+    private Connection con;
+    public ItemDAO() throws DAOException {
+        getConnection();
+    }
+
+
+    public List<Clothes>findall() throws DAOException {
+        if(con==null)
+            getConnection();
+        PreparedStatement st=null;
+        ResultSet rs=null;
+        try{
+            String sql="Select * From item";
+            st=con.prepareStatement(sql);
+            rs=st.executeQuery();
+            List<Clothes>list=new ArrayList<Clothes>();
+            while(rs.next()){
+                int code=rs.getInt("code");
+                String name=rs.getString("name");
+                int price=rs.getInt("price");
+                Clothes bean=new Clothes();
+                bean.setName(name);
+                bean.setPrice(price);
+                list.add(bean);
+            }
+            return list;
+        }catch(Exception e){
+            e.printStackTrace();
+            throw new DAOException("レコードの取得に失敗しました");
+        }finally{
+            try{
+                if(rs!=null)rs.close();
+                if(st!=null)st.close();
+                close();
+            }catch(Exception e){
+                throw new DAOException("リソースの解放に失敗しました");
+            }
+        }
+    }
+
+    private void getConnection()throws DAOException {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost/sample?user=student&password=himitu");
+        }
+        catch(Exception e){
+            throw new DAOException("接続に失敗しました");
+        }
+    }
+
+    private void close() throws SQLException{
+        if(con!=null){
+            con.close();
+            con=null;
+        }
+    }
+}
